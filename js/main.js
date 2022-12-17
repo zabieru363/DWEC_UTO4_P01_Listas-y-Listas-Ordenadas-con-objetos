a7d7bafcca2427710142b30bbc2631f60d7a1764
zabieru363 <zabierujlc@gmail.com>
2022-12-17 19:16:37 +0100

    Clase ObjectList creada

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
 "use strict";
 
+/**
+ * Clase List que crea una colección que permite
+ * trabajar con cualquier tipo de valor.
+ * @author Javier López
+ * @version 1.0
+ */
 class List {
     // Propiedades privadas:
     #list;
+    #capacity = 5;  // Tamaño máximo de la lista.
 
     constructor(list = []) {
+        this.#list = list;
+    }
+}
+
+/**
+ * Subclase de List que contiene una lista que
+ * admite objetos según el tipo de objeto que se
+ * le pase al constructor.
+ * @author Javier López
+ * @version 1.0
+ */
+class ObjectList extends List {
+    // Propiedades privadas.
+    #list;
+    #tyoe;
+
+    constructor(type, list = []) {
+        super(list);
+        this.#tyoe = type;
         this.#list = list;
     }
 }
